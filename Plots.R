d1e7aff4e6d9ed2978a49c929d6485735a591b3f
Sylvia-123 <66501315+Sylvia-123@users.noreply.github.com>
2020-07-21 16:47:23 +1000

    change x,y axis format

--- Plots.R
+++ Plots.R
@@ -3,26 +3,37 @@
 install.packages("interactions")
 library(interactions)
 library(ggplot2)
-theme_set(
-  theme_bw() 
-)
+
+#theme_set(
+# theme_bw() 
+#)
+
+#Change settings regarding theme here
+apatheme=theme_bw()+
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.border=element_blank(),
+        axis.line=element_line(),
+        text=element_text(family='Times'),
+        strip.background = element_rect(fill="white"))
+#, legend.title=element_blank())
 
 #covid distress
 
 ggplot(covid_data_child, aes(x = totalcov_dist, y = SDQhyp2)) +
 geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
 geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Total COVID Negative Impact", y = "Change in Inattention-Hyperactivity")
+  labs(x = "Total COVID Negative Impact", y = "Change in Inattention-Hyperactivity") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = totalcov_dist, y = SDQcon2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Total COVID Negative Impact", y = "Change in Conduct Problems")
+  labs(x = "Total COVID Negative Impact", y = "Change in Conduct Problems") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = totalcov_dist, y = totalPTSD)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Total COVID Negative Impact", y = "Trauma Symptoms")
+  labs(x = "Total COVID Negative Impact", y = "Trauma Symptoms")  + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = totalcov_dist, y = SDQemo2, color = SDQemo_cat, fill = SDQemo_cat)) +
   geom_jitter(size = 1.5, shape = 23) +
@@ -38,44 +49,44 @@ ggplot(covid_data_child, aes(x = totalcov_dist, y = SDQemo2, color = SDQemo_cat,
 ggplot(covid_data_child, aes(x = covid_pos_num, y = SDQpeer2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Total COVID Positive Impact", y = "Change in Peer Problems")
+  labs(x = "Total COVID Positive Impact", y = "Change in Peer Problems")+ apatheme + theme(aspect.ratio = 1)
 
 # DASS
 
 ggplot(covid_data_child, aes(x = DASSDep, y = SDQemo2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Parent Depressive Symptoms", y = "Change in Emotional Problems")
+  labs(x = "Parent Depressive Symptoms", y = "Change in Emotional Problems") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = DASSDep, y = totalPTSD)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Parent Depressive Symptoms", y = "Trauma Symptoms")
+  labs(x = "Parent Depressive Symptoms", y = "Trauma Symptoms") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = DASSAnx, y = SDQemo2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Parent Anxiety Symptoms", y = "Change in Emotional Problems")
+  labs(x = "Parent Anxiety Symptoms", y = "Change in Emotional Problems") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = DASSDep, y = SDQpeer2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Parent Anxiety Symptoms", y = "Change in Peer Problems")
+  labs(x = "Parent Anxiety Symptoms", y = "Change in Peer Problems") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = DASSDep, y = totalPTSD)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Parent Depressive Symptoms", y = "Trauma Symptoms")
+  labs(x = "Parent Depressive Symptoms", y = "Trauma Symptoms") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = DASSStress, y = SDQpeer2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Parent Stress", y = "Change in Peer Problems")
+  labs(x = "Parent Stress", y = "Change in Peer Problems") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = DASSStress, y = totalPTSD)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Parent Stress", y = "Trauma Symptoms")
+  labs(x = "Parent Stress", y = "Trauma Symptoms") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = DASSStress, y = SDQemo2, color = SDQemo_cat, fill = SDQemo_cat)) +
   geom_jitter(size = 1.5, shape = 23) +
@@ -119,12 +130,12 @@ ggplot(covid_data_child, aes(x = DASSStress, y = SDQhyp2, color = ch_age_cat, fi
 ggplot(covid_data_child, aes(x = PARQhostile, y = SDQhyp2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Parental Hostility", y = "Change in Inattention-Hyperactivity")
+  labs(x = "Parental Hostility", y = "Change in Inattention-Hyperactivity") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = PARQhostile, y = totalPTSD)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Parental Hostility", y = "Trauma Symptoms")
+  labs(x = "Parental Hostility", y = "Trauma Symptoms") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = PARQhostile, y = SDQcon2, color = SDQcon_cat, fill = SDQcon_cat)) +
   geom_jitter(size = 1.5, shape = 23) +
@@ -140,58 +151,58 @@ ggplot(covid_data_child, aes(x = PARQhostile, y = SDQcon2, color = SDQcon_cat, f
 ggplot(covid_data_child, aes(x = PARQwarmth, y = totalPTSD)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Low Parental Warmth", y = "Trauma Symptoms")
+  labs(x = "Low Parental Warmth", y = "Trauma Symptoms") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = totalFES, y = totalPTSD)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Family Cohesion", y = "Trauma Symptoms")
+  labs(x = "Family Cohesion", y = "Trauma Symptoms") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = totalFES, y = SDQpeer2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Family Cohesion", y = "Change in Peer Problems")
+  labs(x = "Family Cohesion", y = "Change in Peer Problems") + apatheme + theme(aspect.ratio = 1)
 
 # Communication
 
 ggplot(covid_data_child, aes(x = emotion_comm, y = totalPTSD)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Emotion-focused Communication", y = "Trauma Symptoms")
+  labs(x = "Emotion-focused Communication", y = "Trauma Symptoms") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = emotion_comm, y = SDQhyp2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Emotion-focused Communication", y = "Change in Inattention-Hyperactivity Problems")
+  labs(x = "Emotion-focused Communication", y = "Change in Inattention-Hyperactivity Problems") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = emotion_comm, y = SDQemo2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Emotion-focused Communication", y = "Change in Emotional Problems")
+  labs(x = "Emotion-focused Communication", y = "Change in Emotional Problems") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = emotion_comm, y = SDQpeer2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Emotion-focused Communication", y = "Change in Peer Problems")
+  labs(x = "Emotion-focused Communication", y = "Change in Peer Problems") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = self_comm, y = SDQemo2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Self-focused Communication", y = "Change in Emotional Problems")
+  labs(x = "Self-focused Communication", y = "Change in Emotional Problems") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = self_comm, y = SDQcon2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Self-focused Communication", y = "Change in Conduct Problems")
+  labs(x = "Self-focused Communication", y = "Change in Conduct Problems") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = self_comm, y = SDQhyp2)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Self-focused Communication", y = "Change in Inattention-Hyperactivity Problems")
+  labs(x = "Self-focused Communication", y = "Change in Inattention-Hyperactivity Problems") + apatheme + theme(aspect.ratio = 1)
 
 ggplot(covid_data_child, aes(x = self_comm, y = totalPTSD)) +
   geom_jitter(color = "#00CC99", size = 1.5, shape = 23) +
   geom_smooth(color = "#00AFBB", fill = "#00AFBB", method = lm) +
-  labs(x = "Self-focused Communication", y = "Trauma Symptoms")
+  labs(x = "Self-focused Communication", y = "Trauma Symptoms") + apatheme + theme(aspect.ratio = 1)
 
 
